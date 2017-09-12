183a8b9ac2bdddff2fb2a896aa89c35917b64567
Unknown <null@sattellite.me>
2017-09-12 23:42:24 +0300

    Implemented: constructor, append, head, tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,37 @@
 const Node = require('./node');
 
+// Beginning spied here https://code.tutsplus.com/articles/data-structures-with-javascript-singly-linked-list-and-doubly-linked-list--cms-23392
+
 class LinkedList {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this._head = null;
+    this._tail = null;
+  }
 
-  append(data) {}
+  append(data) {
+    const node = new Node(data);
 
-  head() {}
+    if (this.length) {
+      this._tail.next = node;
+      node.prev = this._tail;
+      this._tail = node;
+    } else {
+      this._head = node;
+      this._tail = node;
+    }
 
-  tail() {}
+    this.length += 1;
+    return node;
+  }
+
+  head() {
+    return this._head.data;
+  }
+
+  tail() {
+    return this._tail.data;
+  }
 
   at(index) {}
 
